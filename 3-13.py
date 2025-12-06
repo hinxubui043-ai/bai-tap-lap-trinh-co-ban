237f2f26adb84c695c4b2057dcefe69c98904430
hinxubui043-ai <hinxubui043@gmail.com>
2025-12-06 09:57:36 +0700

    Update 3-13.py

--- 3-13.py
+++ 3-13.py
@@ -1,3 +1,9 @@
+print("sinh vien : bui hin xu")
+
+print("ma so sv :245751030110041")
+
+print("#############################")
 ds = input("Nhập danh sách: ").split()
 
 print("Vị trí của chuỗi abc là:", ds.index('abc'))
+
