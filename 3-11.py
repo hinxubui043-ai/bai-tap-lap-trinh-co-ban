67c7f8ab7bde047d6c68a67c66437eced3d72269
hinxubui043-ai <hinxubui043@gmail.com>
2025-12-06 09:57:15 +0700

    Update 3-11.py

--- 3-11.py
+++ 3-11.py
@@ -1,6 +1,12 @@
+print("sinh vien : bui hin xu")
+
+print("ma so sv :245751030110041")
+
+print("#############################")
 ds = input("Nhập danh sách: ").split()
 
 ds.append('abc')      # thêm phần tử vào cuối list
 
 for ch in ds:
     print(ch)
+
