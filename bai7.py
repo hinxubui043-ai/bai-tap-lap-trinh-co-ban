291f0eb17ed43feff74c2c16081854573b9b8571
hinxubui043-ai <hinxubui043@gmail.com>
2025-12-06 09:52:32 +0700

    Update bai7.py

--- bai7.py
+++ bai7.py
@@ -1,5 +1,11 @@
+print("sinh vien : bui hin xu")
+
+print("ma so sv :245751030110041")
+
+print("#############################")
 n=int(input('nhap mot so:'))
 d=dict()
 for i in range(1,n+1):
     d[i]=i*i
 print (d)
+
