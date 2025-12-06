91b80e20c49526e3a4629fac116c46be04bfe8d1
hinxubui043-ai <hinxubui043@gmail.com>
2025-12-06 10:01:14 +0700

    Update 3-8.py

--- 3-8.py
+++ 3-8.py
@@ -1,3 +1,8 @@
+print("sinh vien : bui hin xu")
+
+print("ma so sv :245751030110041")
+
+print("#############################")
 ds = input("Nhập dãy từ: ").split()
 
 maxlen = max(len(t) for t in ds)
@@ -5,3 +10,4 @@ maxlen = max(len(t) for t in ds)
 for t in ds:
     if len(t) == maxlen:
         print(t)
+
