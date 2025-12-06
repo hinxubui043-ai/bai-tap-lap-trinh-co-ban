24a2fc0554240199acd5a46ef557d13db7407933
hinxubui043-ai <hinxubui043@gmail.com>
2025-12-06 09:58:14 +0700

    Update 3-16.py

--- 3-16.py
+++ 3-16.py
@@ -1,5 +1,11 @@
+print("sinh vien : bui hin xu")
+
+print("ma so sv :245751030110041")
+
+print("#############################")
 s = input("Nhập chuỗi nhị phân: ")
 lst = s.split(',')
 
 for val in lst:
     print(val)
+
