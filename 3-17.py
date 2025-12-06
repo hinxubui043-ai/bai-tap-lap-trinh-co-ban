f79710a71a7d5ae87354b7ef2e557ec1c630b940
hinxubui043-ai <hinxubui043@gmail.com>
2025-12-06 09:58:25 +0700

    Update 3-17.py

--- 3-17.py
+++ 3-17.py
@@ -1,5 +1,11 @@
+print("sinh vien : bui hin xu")
+
+print("ma so sv :245751030110041")
+
+print("#############################")
 n = int(input("Nhập n: "))
 
 for i in range(1, n+1):
     if n % i == 0:
         print(i)
+
