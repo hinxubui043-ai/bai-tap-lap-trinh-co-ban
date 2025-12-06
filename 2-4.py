46005f80a84a2d8675a52f51995aa4897a53f5b1
hinxubui043-ai <hinxubui043@gmail.com>
2025-12-06 09:54:55 +0700

    Update 2-4.py

--- 2-4.py
+++ 2-4.py
@@ -1,6 +1,12 @@
+print("sinh vien : bui hin xu")
+
+print("ma so sv :245751030110041")
+
+print("#############################")
 a="hello guy"
 def say(a):
     a="vinh university"
     print(a)
 say(a)
 print(a)
+
