3a9c148f8e313d1a761da5922db3f31c3f6abae4
hinxubui043-ai <hinxubui043@gmail.com>
2025-12-06 10:00:55 +0700

    Update 3-6.py

--- 3-6.py
+++ 3-6.py
@@ -1,5 +1,11 @@
+print("sinh vien : bui hin xu")
+
+print("ma so sv :245751030110041")
+
+print("#############################")
 hoten = input("Nhập họ tên: ").split()
 ho = hoten[0]
 ten = hoten[1]
 print("Họ:", ho)
 print("Tên:", ten)
+
