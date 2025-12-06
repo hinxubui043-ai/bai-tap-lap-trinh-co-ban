5b5022c467c2f1a8c0edb69f9bfead0b6b488246
hinxubui043-ai <hinxubui043@gmail.com>
2025-12-06 09:51:38 +0700

    Update bai3.py

--- bai3.py
+++ bai3.py
@@ -1,5 +1,11 @@
+print("sinh vien : bui hin xu")
+
+print("ma so sv :245751030110041")
+
+print("#############################")
 n =int(input("enter a number--->"))
 if n% 2==0:
     print ("even number");
 else:
     print ("ODD Number");
+
