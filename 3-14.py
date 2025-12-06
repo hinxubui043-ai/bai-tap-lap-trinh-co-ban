60339c15c287440225bb531a8c2664a63ce51e9e
hinxubui043-ai <hinxubui043@gmail.com>
2025-12-06 09:57:46 +0700

    Update 3-14.py

--- 3-14.py
+++ 3-14.py
@@ -1,6 +1,12 @@
+print("sinh vien : bui hin xu")
+
+print("ma so sv :245751030110041")
+
+print("#############################")
 ds = input("Nhập danh sách: ").split()
 
 ds.sort()     # sắp xếp theo thứ tự từ điển
 
 for ch in ds:
     print(ch)
+
