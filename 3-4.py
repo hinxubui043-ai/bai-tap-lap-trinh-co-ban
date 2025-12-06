ee9ad202ba0976da8f9af088c1359d8c619fc9a3
hinxubui043-ai <hinxubui043@gmail.com>
2025-12-06 10:00:37 +0700

    Update 3-4.py

--- 3-4.py
+++ 3-4.py
@@ -1,5 +1,11 @@
+print("sinh vien : bui hin xu")
+
+print("ma so sv :245751030110041")
+
+print("#############################")
 ds=input('danh sách :').split()
 print(ds)
 
 for so in ds:
     print(so)
+
