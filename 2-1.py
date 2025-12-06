f1cd37f5a5940e0bed634b54eafaecb1e17e82f2
hinxubui043-ai <hinxubui043@gmail.com>
2025-12-06 09:54:20 +0700

    Update 2-1.py

--- 2-1.py
+++ 2-1.py
@@ -1,6 +1,12 @@
+print("sinh vien : bui hin xu")
+
+print("ma so sv :245751030110041")
+
+print("#############################")
 def sum(a,b):
     print ("sum="+str(a+b))
 #tinh tong 2 so 4,5
 sum(4,5)
 #tinh tong 2 so 3,7
 sum(3,7)
+
