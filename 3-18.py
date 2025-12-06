0c872c226290939d6fcf9bcb3bced468604d670b
hinxubui043-ai <hinxubui043@gmail.com>
2025-12-06 09:58:37 +0700

    Update 3-18.py

--- 3-18.py
+++ 3-18.py
@@ -1,3 +1,8 @@
+print("sinh vien : bui hin xu")
+
+print("ma so sv :245751030110041")
+
+print("#############################")
 n = int(input("Nhập n: "))
 
 fib = [0, 1]
@@ -6,3 +11,4 @@ for i in range(2, n):
     fib.append(fib[-1] + fib[-2])
 
 print(fib[:n])
+
