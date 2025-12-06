7fd62014ab68ffa5ce98cd407c9baec3b22e14ef
hinxubui043-ai <hinxubui043@gmail.com>
2025-12-06 09:59:00 +0700

    Update 3-2.py

--- 3-2.py
+++ 3-2.py
@@ -1,5 +1,11 @@
+print("sinh vien : bui hin xu")
+
+print("ma so sv :245751030110041")
+
+print("#############################")
 S=input('nhap chuoi:')
 for ch in S :
     if ch == ' ' or ch =='\t':
         continue
     print(ch)
+
