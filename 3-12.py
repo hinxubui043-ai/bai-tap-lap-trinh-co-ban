7b9680093f928580b1e8acb9be838150926c43e0
hinxubui043-ai <hinxubui043@gmail.com>
2025-12-06 09:57:25 +0700

    Update 3-12.py

--- 3-12.py
+++ 3-12.py
@@ -1,6 +1,12 @@
+print("sinh vien : bui hin xu")
+
+print("ma so sv :245751030110041")
+
+print("#############################")
 ds = input("Nhập danh sách: ").split()
 
 ds.remove('123')      # xóa phần tử có giá trị '123'
 
 for ch in ds:
     print(ch)
+
