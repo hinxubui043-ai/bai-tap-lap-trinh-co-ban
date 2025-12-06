8116ccf9cc8cd8d038d7c04b3ee17d6f2e9fa27d
hinxubui043-ai <hinxubui043@gmail.com>
2025-12-06 09:52:58 +0700

    Update bai9.py

--- bai9.py
+++ bai9.py
@@ -1,3 +1,8 @@
+print("sinh vien : bui hin xu")
+
+print("ma so sv :245751030110041")
+
+print("#############################")
 str = input("enter a string:")
 dict={}
 for n in str:
@@ -7,3 +12,4 @@ for n in str:
     else:
         dict[n]=1
         print(dict)
+
