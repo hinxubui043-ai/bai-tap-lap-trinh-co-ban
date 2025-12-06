38e2fd547c9244c7af2f5d0c565fa2d11b832aaa
hinxubui043-ai <hinxubui043@gmail.com>
2025-12-06 09:52:46 +0700

    Update bai8.py

--- bai8.py
+++ bai8.py
@@ -1,3 +1,8 @@
+print("sinh vien : bui hin xu")
+
+print("ma so sv :245751030110041")
+
+print("#############################")
 a,b=1,2
 total=0
 while (a<=4000000-1):
@@ -6,3 +11,4 @@ while (a<=4000000-1):
     a,b =b,a+b
     print(a,end=" " )
 print("\n sum of prime numbers term in fibonacci seriews:",total)
+
