5a2e1bc656b6f977497c4d54921f399982362caa
hinxubui043-ai <hinxubui043@gmail.com>
2025-12-06 09:52:09 +0700

    Update bai5.py

--- bai5.py
+++ bai5.py
@@ -1,4 +1,10 @@
+print("sinh vien : bui hin xu")
+
+print("ma so sv :245751030110041")
+
+print("#############################")
 n=int(input("enter A number--->"));
 while n>=0:
           print (n);
           n=n-1;
+
