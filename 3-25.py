8042d1068b022f17a8a8cbfe0d7a75909ba1c292
hinxubui043-ai <hinxubui043@gmail.com>
2025-12-06 10:00:07 +0700

    Update 3-25.py

--- 3-25.py
+++ 3-25.py
@@ -1,3 +1,8 @@
+print("sinh vien : bui hin xu")
+
+print("ma so sv :245751030110041")
+
+print("#############################")
 s = input("Nhập danh sách số (cách nhau bởi dấu phẩy): ")
 
 nums = [int(x) for x in s.split(',')]
@@ -5,3 +10,4 @@ nums = [int(x) for x in s.split(',')]
 odd = [str(x) for x in nums if x % 2 == 1]
 
 print(",".join(odd))
+
