c79ab81ce4b9ebf00a0c76fb244dca9e5f7f4c7b
hinxubui043-ai <hinxubui043@gmail.com>
2025-12-06 09:56:33 +0700

    Update 2-11.py

--- 2-11.py
+++ 2-11.py
@@ -1,3 +1,8 @@
+print("sinh vien : bui hin xu")
+
+print("ma so sv :245751030110041")
+
+print("#############################")
 def benefit(t, n, k):
     """Tính tổng tiền (lãi kép) sau k tháng. t là lãi suất %/tháng, n là vốn, k là số tháng."""
     
@@ -31,3 +36,4 @@ try:
         
 except ValueError:
     print("Lỗi: Vui lòng nhập các giá trị là số hợp lệ.")
+
