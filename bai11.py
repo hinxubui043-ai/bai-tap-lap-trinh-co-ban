8d5c87032eb3b2133097ac9b22e580252324a2e4
hinxubui043-ai <hinxubui043@gmail.com>
2025-12-06 09:53:50 +0700

    Update bai11.py

--- bai11.py
+++ bai11.py
@@ -1,3 +1,8 @@
+print("sinh vien : bui hin xu")
+
+print("ma so sv :245751030110041")
+
+print("#############################")
 l=[1,'python',4,7]
 k=['cse',2,'guntur',8]
 m=[]
@@ -6,3 +11,4 @@ m.append(k);
 print(m)
 d={1:l,2:k,'combine_list':m}
 print(d)
+
