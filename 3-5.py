30d8667b7a3bbba8e1419d00b5f43d15631dc371
hinxubui043-ai <hinxubui043@gmail.com>
2025-12-06 10:00:46 +0700

    Update 3-5.py

--- 3-5.py
+++ 3-5.py
@@ -1,3 +1,9 @@
+print("sinh vien : bui hin xu")
+
+print("ma so sv :245751030110041")
+
+print("#############################")
 ds=input('danh sach:').split()
 for item in reversed(ds):
     print(item)
+
