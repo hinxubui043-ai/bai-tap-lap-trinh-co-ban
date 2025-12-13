d5da27b34b58e4e593aef9ce64d9fad0189dcb65
hinxubui043-ai <hinxubui043@gmail.com>
2025-12-13 08:28:20 +0700

    Update bai2.py

--- bai2.py
+++ bai2.py
@@ -1,3 +1,8 @@
+print("sinh vien : bui hin xu")
+
+print("ma so sv :245751030110041")
+
+print("#############################")
 class Hinhchunhat(object):
     def __init__(self, dai, rong):
         self.dai = dai
@@ -7,3 +12,4 @@ class Hinhchunhat(object):
         return self.dai * self.rong
 hinh_chu_nhat_moi = Hinhchunhat(8,10)
 print(hinh_chu_nhat_moi.area())
+
