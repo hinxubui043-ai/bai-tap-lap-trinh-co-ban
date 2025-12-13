6ef13244a8bfda81d7e46ca0db53a6e2861d5721
hinxubui043-ai <hinxubui043@gmail.com>
2025-12-13 08:34:39 +0700

    Update bai3.py

--- bai3.py
+++ bai3.py
@@ -1,3 +1,8 @@
+print("sinh vien : bui hin xu")
+
+print("ma so sv :245751030110041")
+
+print("#############################")
 class Nguoi(object):
     def getGender(self):
         return "Unknown"
@@ -13,3 +18,4 @@ aNam = Nam()
 aNu = Nu()
 print(aNam.getGender())
 print(aNu.getGender())
+
