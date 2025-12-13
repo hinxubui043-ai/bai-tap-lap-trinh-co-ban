d235e7ff00fab47c336bfc0bb5ba7e49eb35dc54
hinxubui043-ai <hinxubui043@gmail.com>
2025-12-13 08:28:51 +0700

    Update bai5.py

--- bai5.py
+++ bai5.py
@@ -1,5 +1,11 @@
+print("sinh vien : bui hin xu")
+
+print("ma so sv :245751030110041")
+
+print("#############################")
 class py_solution:
     def reverse_words(self, s):
         return ' '.join(reversed(s.split()))
 
 print(py_solution().reverse_words("hello .py"))
+
